b88985cce6cfebadd71a1e9514e53fa03bb21733
Arif Hossain <hossaininfo@ymail.com>
2025-03-10 12:00:04 +0100

    added router link to index.js

--- src/router/index.js
+++ src/router/index.js
@@ -12,10 +12,27 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/layout',
+      name: 'layout',
+      component: () => import('../components/LayOut.vue'),
+    },
+    {
+      path: '/project',
+      name: 'project',
+      component: () => import('../views/ProjectsView.vue'),
+    },
+    {
+      path: '/experience',
+      name: 'experience',
+      component: () => import('../views/ExperienceView.vue'),
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/ContactView.vue'),
     },
   ],
 })
