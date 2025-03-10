3e5daa857ca0875c1c7217f0bf9930ee52a9a99d
Arif Hossain <hossaininfo@ymail.com>
2025-03-10 12:05:48 +0100

    created empty home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
+<script setup></script>
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Hello from home</h1>
+  </div>
 </template>
+<style scoped></style>
