05e7150d38e0ebd501fe82e1bba9e1c9aae61e2e
Arif Hossain <hossaininfo@ymail.com>
2025-03-10 12:00:37 +0100

    added some dummy data to experiences.js and projects.js

---
+++ src/stores/experiences.js
@@ -0,0 +1,132 @@
+export const experiences = [
+  {
+    title: 'Full-stack Developer',
+    company: 'Experis Academy',
+    type: 'Full-time',
+    duration: 'Jan 2023 - Jun 2023 · 6 mos',
+    location: 'Stockholm, Sweden · On-site',
+    projects: [
+      {
+        name: 'Project ‘Lagalt’ / Full-stack Developer',
+        period: 'April 2023 - June 2023',
+        location: 'Stockholm',
+        technologies: ['Java', 'Spring Boot', 'ReactJS', 'Git', 'Docker', 'Keycloak'],
+        description: `
+          Played a pivotal role in developing 'Project Lagalt,' a web platform for creative professionals.
+          Led backend development using Java and Spring Boot and contributed to front-end enhancements with Bootstrap.
+          Key tasks included OAuth integration, privacy management, and GitHub project collaboration.
+          Successfully presented the project to 47 developers and stakeholders.
+        `,
+        showMore: false,
+      },
+      {
+        name: 'Project ‘Movie Characters API’ / Backend Developer',
+        period: 'February 2023 - March 2023',
+        location: 'Stockholm',
+        technologies: [
+          'Java 17',
+          'Spring Boot 3',
+          'Maven',
+          'Hibernate',
+          'PostgreSQL',
+          'Spring Data JPA',
+          'Spring Web',
+          'Project Lombok',
+          'Validation API',
+          'MapStruct',
+          'Springdoc OpenAPI',
+          'Swagger',
+          'Docker',
+          'Git',
+          'IntelliJ',
+          'Postman',
+        ],
+        description: `
+          Developed a CRUD API managing movie character data in a PostgreSQL database using Hibernate.
+          Focused on API documentation, testing, and pair programming in a two-person team over two weeks.
+        `,
+        showMore: false,
+      },
+    ],
+  },
+  {
+    title: 'Full-stack Developer',
+    company: 'Hexa Studio AB',
+    type: 'Internship',
+    duration: 'May 2022 - Nov 2022 · 7 mos',
+    location: 'Stockholm, Sweden · Remote',
+    projects: [
+      {
+        name: 'Project ‘EntryId’ / Backend Developer & Project Leader',
+        period: 'May 2022 - June 2022',
+        location: 'Stockholm',
+        technologies: [
+          'Java',
+          'Spring Boot',
+          'Spring Security',
+          'Git',
+          'Docker',
+          'CI/CD',
+          'JWT Auth',
+          'Postman',
+        ],
+        description: `
+          Led the backend development for 'Project EntryId,' an application optimizing restaurant branch operations.
+          Developed a secure login system, scheduling feature, and budgeting module over six weeks.
+          Leveraged restaurant management experience to ensure practical application usability.
+        `,
+        showMore: false,
+      },
+      {
+        name: 'Project ‘KitsConstructor’ / Full-stack Developer',
+        period: 'July 2022 - September 2022',
+        location: 'Stockholm',
+        technologies: [
+          'Java',
+          'Spring Boot',
+          'Spring Security',
+          'Git',
+          'Docker',
+          'CI/CD',
+          'JWT Auth',
+          'ReactJS',
+          'React Native',
+          'Xcode',
+          'IntelliJ',
+          'WebStorm',
+          'Postman',
+        ],
+        description: `
+          Led digital transformation of a recycling company by developing an admin page and a user-friendly mobile app.
+          Focused on adaptability to changing client requirements while ensuring a clean and scalable codebase.
+        `,
+        showMore: false,
+      },
+    ],
+  },
+  {
+    title: 'Restaurant Manager',
+    company: 'VIGÅRDA VASAGATAN 44 AB',
+    type: 'Full-time',
+    duration: 'Sep 2017 - 2020 · 2 yrs 5 mos',
+    location: 'Stockholm, Sweden · On-site',
+    description: `
+      Managed overall restaurant operations at Vigårda, including staff management, budgeting, and overseeing daily sales.
+      Successfully balanced owner expectations, team dynamics, and customer satisfaction, leading to improved sales and efficiency.
+    `,
+    showMore: false,
+  },
+  {
+    title: 'Shift Manager',
+    company: "McDonald's",
+    type: 'Full-time',
+    duration: '2010 - 2016 · 6 yrs',
+    location: 'Stockholm, Sweden · On-site',
+    description: `
+      Managed operations at McDonald's Tornado AB, including ordering raw materials, scheduling, and equipment maintenance.
+      Recognized for problem-solving skills, handling guest claims, and being awarded 'Employee of the Month' multiple times.
+      Completed advanced management courses at McDonald's Business School.
+    `,
+    showMore: false,
+  },
+]
